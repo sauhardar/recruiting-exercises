0613d19c4b6d98f55efa5246a085ab94e40b2490
Sauharda Rajbhandari <sauharda19@gmail.com>
2020-08-25 17:06:13 -0400

    Add more tests

--- inventory-allocator/src/index.ts
+++ inventory-allocator/src/index.ts
@@ -21,27 +21,32 @@ class InventoryAllocator {
    * @param order The requirements from the customer.
    * @param inventory What is current in stock and where.
    */
-  static getCheapestShipment(order: Order, inventory: Inventory ): WarehouseFulfillment[] {
+  static getCheapestShipment(order: Order, warehouses: Inventory ): WarehouseFulfillment[] {
     const remainingOrder = order;
     const output: WarehouseFulfillment[] = [];
 
-    inventory.forEach((warehouse) => {
+    warehouses.forEach((warehouse) => {
       const { name: warehouseName, inventory } = warehouse;
 
       const warehouseFulfillment: WarehouseFulfillment = { [warehouseName]: {} };
       
-      Object.keys(remainingOrder).map(function(itemName, index) {
-        if (itemName in inventory && inventory[itemName] !== 0 && remainingOrder[itemName] != 0) {
+      Object.keys(remainingOrder).map(function(itemName) {
+        if (itemName in inventory && inventory[itemName] !== 0 && remainingOrder[itemName] > 0) {
           const itemsAdded: number = inventory[itemName] >= remainingOrder[itemName] 
           ? remainingOrder[itemName] : inventory[itemName];
 
           warehouseFulfillment[warehouseName][itemName] = itemsAdded
 
           remainingOrder[itemName] -= itemsAdded;
+
+          if (remainingOrder[itemName] === 0) {
+            delete remainingOrder[itemName]
+          }
         }
       });
+
       Object.keys(warehouseFulfillment[warehouseName]).length !== 0 
-      && output.push(warehouseFulfillment);
+        && output.push(warehouseFulfillment);
     })
 
     let isOrderFulfilled: boolean = true;
@@ -56,13 +61,3 @@ class InventoryAllocator {
 }
 
 export default InventoryAllocator;
-
-console.log(InventoryAllocator.getCheapestShipment({apple: 5, bananas: 10, chocolate: 15}, 
-  [{ name: 'A', inventory: { apple: 5 } }, 
-  { name: 'B', inventory: { apple: 5, bananas: 7, chocolate: 10} },
-  { name: 'C', inventory: { apple: 10, bananas: 4, chocolate: 20} }]))
-
-  console.log(InventoryAllocator.getCheapestShipment({ apple: 1 }, 
-    [{ name: 'owd', inventory: { apple: 0 } }]))
-
-    console.log('asdf', )
--- inventory-allocator/tests/main.test.ts
+++ inventory-allocator/tests/main.test.ts
@@ -1,8 +1,12 @@
 import InventoryAllocator from '../src'
 
-test('should pass happy case where 1 apple is required and 1 apple is available', () => {
+test('should pass happy case where enough resources are available', () => {
   expect(InventoryAllocator.getCheapestShipment({ apple: 1 }, 
     [{ name: 'owd', inventory: { apple: 1 } }])).toEqual([ { owd: { apple: 1 } } ]);
+    
+  expect(InventoryAllocator.getCheapestShipment({ banana: 10 }, 
+    [{ name: 'A', inventory: { banana: 10 } }, {
+      name: 'B', inventory: { apple: 10, banana: 10 } }])).toEqual([ { A: { banana: 10 } } ]);
 });
 
 test('should return no allocations', () => {
@@ -15,6 +19,10 @@ test('should return no allocations', () => {
   expect(InventoryAllocator.getCheapestShipment({ apple: 3, banana: 2, chocolate: 1 }, 
     [{ name: 'A', inventory: { apple: 1, banana: 1, chocolate: 0 } },
      { name: 'B', inventory: { apple: 0, banana: 1, chocolate: 0 }}])).toEqual([]);
+
+  expect(InventoryAllocator.getCheapestShipment({ apple: 3, banana: 2, chocolate: 1 }, 
+  [{ name: 'A', inventory: { apple: 1, banana: 10, chocolate: 10 } },
+    { name: 'B', inventory: { apple: 1, banana: 10, chocolate: 10 }}])).toEqual([]);
 })
 
 test('should split an item across warehouses if that is the only way to completely ship an item', 
@@ -29,6 +37,17 @@ test('should split an item across warehouses if that is the only way to complete
       { name: 'C', inventory: { apple: 10, bananas: 4, chocolate: 20} }])).toEqual([
         { A: { apple: 5} }, { B: { bananas: 7, chocolate: 10 } }, { C: { bananas: 3, chocolate: 5 }} 
       ]);
+
+    expect(InventoryAllocator.getCheapestShipment({apple: 5, banana: 10, chocolate: 15}, 
+      [{ name: 'A', inventory: { apple: 5}}, { name: 'B', inventory: {banana: 10}},
+       {name: 'C', inventory: {chocolate: 15}}])).toEqual([{A: { apple: 5}}, {B: {banana: 10}},
+      {C: {chocolate: 15}}])
+
+    // Warehouse C only has 14 chocolate items whereas 15 are needed, 
+    // so it is an unfulfilled shipment
+    expect(InventoryAllocator.getCheapestShipment({apple: 5, banana: 10, chocolate: 15}, 
+    [{ name: 'A', inventory: { apple: 5}}, { name: 'B', inventory: {banana: 10}},
+    {name: 'C', inventory: {chocolate: 14}}])).toEqual([]) 
 
     // !! currently fails bc of warehouse name order
     // expect(InventoryAllocator.getCheapestShipment({ apple: 10 }, 
